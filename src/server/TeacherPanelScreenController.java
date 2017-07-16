65fcdd4f159b79603b314dd16b574ce166323cd5
Jakub <524umc@gmail.com>
2017-07-16 20:28:41 +0200

    Added TeacherPanelScreenController and TeacherPanelScreen.fxml

---
+++ src/server/TeacherPanelScreenController.java
@@ -0,0 +1,10 @@
+package server;
+
+public class TeacherPanelScreenController {
+	
+	MainScreenController mainScreenController;
+	
+	public void setMainController(MainScreenController mainScreenController) {
+		this.mainScreenController = mainScreenController;
+	}
+}
