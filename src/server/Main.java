db90ca4024372425b3aacbd5fc221f8e6c817c70
Jakub <524umc@gmail.com>
2017-07-16 09:58:50 +0200

    corrected server Main.java

--- src/server/Main.java
+++ src/server/Main.java
@@ -2,17 +2,18 @@ package server;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("MainScreen.fxml"));
+        Pane pane = loader.load();
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
